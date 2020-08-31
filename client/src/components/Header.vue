33a68b27e39b18714664954ade9f24d0e1e9e092
LDD <leonardodagios@gmai.com>
2020-08-30 22:39:07 -0400

    Criado componente de cabecalho

---
+++ client/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+        <nav class="navigation">
+            <a href="feed.html">
+                <img 
+                    src="images/navLogo.png"
+                    alt="logo"
+                    title="logo"
+                    class="navigation__logo"
+                />
+            </a>
+            <div class="navigation__search-container">
+                <i class="fa fa-search"></i>
+                <input type="text" placeholder="Search">
+            </div>
+            <div class="navigation__icons">
+                <a href="explore.html" class="navigation__link">
+                    <i class="fa fa-compass"></i>
+                </a>
+                <a href="#" class="navigation__link">
+                    <i class="fa fa-heart-o"></i>
+                </a>
+                <a href="profile.html" class="navigation__link">
+                    <i class="fa fa-user-o"></i>
+                </a>
+            </div>
+        </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+</style>
